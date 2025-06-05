9d8cef89a13e44c6c6d62cfab2eff7c532ef3c74
Nardos0915 <nardoslegese0915@gmail.com>
2025-06-05 17:09:10 +0300

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5555/api',
+  baseURL:'https://book-store-app-5.onrender.com/api',
 });
 
 // Add a request interceptor
